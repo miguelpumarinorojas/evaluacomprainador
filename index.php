672c4180cb6ed10b80a983d2ece6ee3afcddfb04
miguelpumarinorojas <m.pumarinorojas@uandresbello.edu>
2026-08-29 12:28:56 -0400

    probando de nuevo que se actualice el cambio con .cpanel.yml

--- index.php
+++ index.php
@@ -7,6 +7,9 @@ session_variable('');
 
 <!doctype html>
 <html lang="en">
+
+algun cambio menor
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
